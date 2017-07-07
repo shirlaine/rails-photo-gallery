358b291cdb0ec799999477f3cf5e0d1fca1f24a8
shirlaine <shirlaine.phang@gmail.com>
2017-07-07 22:07:10 +0800

    Add create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,20 @@ class PhotosController < ApplicationController
 		@photo = Photo.new
 	end 
 
+	def create
+		@photo = Photo.new(photo_params)
+		@photo.save
+		redirect_to photos_url
+	end
+
+	def show
+		@photo = Photo.find(params[:id])
+	end	
+
+private
+
+	def photo_params
+		params.require(:photo).permit(:title, :date, :description, :file_location)
+	end
+
 end
