fe080abdb97a2355742c4baeb982ec44716139d6
shirlaine <shirlaine.phang@gmail.com>
2017-07-11 16:59:08 +0800

    Add Edit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -18,6 +18,25 @@ class PhotosController < ApplicationController
 		@photo = Photo.find(params[:id])
 	end	
 
+	def edit
+		@photo = Photo.find(params[:id])
+	end	
+
+	def update
+		@photo = Photo.find(params[:id])
+		@photo.update_attributes(photo_params)
+
+		redirect_to photo_path(@photo)
+	end 
+
+	def destroy
+		@photo = Photo.find(params[:id])
+		@photo.destroy 
+
+		redirect_to photos_url
+	end
+
+
 private
 
 	def photo_params
