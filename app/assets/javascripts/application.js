5d7b010f3bfe8e6bf6b66064870d103d0f482ade
shirlaine <shirlaine.phang@gmail.com>
2017-07-14 14:42:05 +0800

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
+
 //= require rails-ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
